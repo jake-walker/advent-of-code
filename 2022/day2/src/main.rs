ec609a46da87d93868691e6cf4527671b3b7ddd9
Jake Walker <hi@jakew.me>
2022-12-02 17:22:51 +0000

    2022 day 2 refactor

--- 2022/day2/src/main.rs
+++ 2022/day2/src/main.rs
@@ -1,11 +1,26 @@
 use std::fs;
 
+// slice with all shapes and outcomes. elements can be found by using 2 values
+// to find the missing one
+const OUTCOMES: [(char, char, i32); 9] = [
+    // p0 shape, p1 shape, outcome (as p1)
+    ('A', 'X', 3), // rock + rock = draw
+    ('A', 'Y', 6), // rock + paper = win
+    ('A', 'Z', 0), // rock + scissors = loss
+    ('B', 'X', 0), // paper + rock = loss
+    ('B', 'Y', 3), // paper + paper = draw
+    ('B', 'Z', 6), // paper + scissors = win
+    ('C', 'X', 6), // scissors + rock = win
+    ('C', 'Y', 0), // scissors + rock = loss
+    ('C', 'Z', 3) // scissors + scissors = draw
+];
+
 fn main() {
     let input = parse_guide(&fs::read_to_string("inputs/input.txt")
             .expect("should be able to read input"));
 
-    println!("Part 1: {}", score_part_1(&input));
-    println!("Part 2: {}", score_part_2(&input));
+    println!("Part 1: {}", input.iter().map(|round| score_part_1(&round)).sum::<i32>());
+    println!("Part 2: {}", input.iter().map(|round| score_part_2(&round)).sum::<i32>());
 }
 
 fn parse_guide(guide: &String) -> Vec<(char, char)> {
@@ -18,6 +33,7 @@ fn parse_guide(guide: &String) -> Vec<(char, char)> {
 }
 
 fn shape_score(shape: &char) -> i32 {
+    // convert a shape into the shape score
     match shape {
         'X' => 1,
         'Y' => 2,
@@ -26,57 +42,33 @@ fn shape_score(shape: &char) -> i32 {
     }
 }
 
-fn outcome_score(round: &(char, char)) -> i32 {
-    // convert the second character to be A, B or C for comparisons
-    let p1 = ((round.1 as u8) - 23) as char;
+fn score_part_1(round: &(char, char)) -> i32 {
+    // find the outcome score by looking up the two played moves
+    let outcome_score = OUTCOMES.iter()
+        .find(|x| x.0 == round.0 && x.1 == round.1)
+        .unwrap().2;
 
-    if round.0 == p1 {
-        // draw
-        3
-    } else if (p1 == 'B' && round.0 == 'A') ||
-            (p1 == 'C' && round.0 == 'B') ||
-            (p1 == 'A' && round.0 == 'C') {
-        // p1 wins
-        6
-    } else {
-        // p0 wins
-        0
-    }
+    // calculate shape score + outcome score
+    shape_score(&round.1) + outcome_score
 }
 
-fn get_shape(round: &(char, char)) -> char {
-    if round.1 == 'Y' {
-        return ((round.0 as u8) + 23) as char;
-    }
+fn score_part_2(round: &(char, char)) -> i32 {
+    // convert x/y/z into an outcome score
+    let outcome_score = {
+        match round.1 {
+            'Y' => 3,
+            'Z' => 6,
+            _ => 0
+        }
+    };
 
-    match round {
-        ('A', 'X') => 'Z', // rock, lose
-        ('B', 'X') => 'X', // paper, lose
-        ('C', 'X') => 'Y', // scissors, lose
-        ('A', 'Z') => 'Y', // rock, win
-        ('B', 'Z') => 'Z', // paper, win
-        ('C', 'Z') => 'X', // scissors, win
-        _ => ' '
-    }
-}
+    // find p1 shape by looking up p0 shape and outcome score
+    let p1 = OUTCOMES.iter()
+        .find(|x| x.0 == round.0 && x.2 == outcome_score)
+        .unwrap().1;
 
-fn score_part_1(guide: &Vec<(char, char)>) -> i32 {
-    guide.iter().map(|round| shape_score(&round.1) + outcome_score(round)).sum()
-}
-
-fn score_part_2(guide: &Vec<(char, char)>) -> i32 {
-    guide.iter().map(|round| {
-        let p1 = get_shape(&round);
-        let outcome = {
-            match round.1 {
-                'Y' => 3,
-                'Z' => 6,
-                _ => 0
-            }
-        };
-
-        shape_score(&p1) + outcome
-    }).sum()
+    // calculate shape score + outcome score
+    shape_score(&p1) + outcome_score
 }
 
 #[cfg(test)]
@@ -89,61 +81,51 @@ mod tests {
     fn parse_guide_example() {
         let example = fs::read_to_string("inputs/example.txt")
             .expect("should be able to read input");
-        assert_eq!(parse_guide(&example), vec![('A', 'Y'), ('B', 'X'), ('C', 'Z')])
+        assert_eq!(parse_guide(&example), vec![('A', 'Y'), ('B', 'X'), ('C', 'Z')]);
     }
 
     #[test]
     fn shape_score_1() {
-        assert_eq!(shape_score(&'Y'), 2)
+        assert_eq!(shape_score(&'Y'), 2);
     }
 
     #[test]
     fn shape_score_2() {
-        assert_eq!(shape_score(&'X'), 1)
+        assert_eq!(shape_score(&'X'), 1);
     }
 
     #[test]
     fn shape_score_3() {
-        assert_eq!(shape_score(&'Z'), 3)
+        assert_eq!(shape_score(&'Z'), 3);
     }
 
     #[test]
-    fn outcome_score_1() {
-        assert_eq!(outcome_score(&('A', 'Y')), 6)
+    fn score_part_1_1() {
+        assert_eq!(score_part_1(&('A', 'Y')), 8);
     }
 
     #[test]
-    fn outcome_score_2() {
-        assert_eq!(outcome_score(&('B', 'X')), 0)
+    fn score_part_1_2() {
+        assert_eq!(score_part_1(&('B', 'X')), 1);
     }
 
     #[test]
-    fn outcome_score_3() {
-        assert_eq!(outcome_score(&('C', 'Z')), 3)
+    fn score_part_1_3() {
+        assert_eq!(score_part_1(&('C', 'Z')), 6);
     }
 
     #[test]
-    fn score_part_1_example() {
-        assert_eq!(score_part_1(&vec![('A', 'Y'), ('B', 'X'), ('C', 'Z')]), 15)
+    fn score_part_2_1() {
+        assert_eq!(score_part_2(&('A', 'Y')), 4);
     }
 
     #[test]
-    fn get_shape_1() {
-        assert_eq!(get_shape(&('A', 'Y')), 'X')
+    fn score_part_2_2() {
+        assert_eq!(score_part_2(&('B', 'X')), 1);
     }
 
     #[test]
-    fn get_shape_2() {
-        assert_eq!(get_shape(&('B', 'X')), 'X')
-    }
-
-    #[test]
-    fn get_shape_3() {
-        assert_eq!(get_shape(&('C', 'Z')), 'X')
-    }
-
-    #[test]
-    fn score_part_2_example() {
-        assert_eq!(score_part_2(&vec![('A', 'Y'), ('B', 'X'), ('C', 'Z')]), 12)
+    fn score_part_2_3() {
+        assert_eq!(score_part_2(&('C', 'Z')), 7);
     }
 }
