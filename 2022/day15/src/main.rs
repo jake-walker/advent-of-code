01370e75f11ea5bc11a72497b71c5a0685ee153f
Jake Walker <hi@jakew.me>
2022-12-15 18:37:54 +0000

    add 2022 day 15 attempt 2

--- 2022/day15/src/main.rs
+++ 2022/day15/src/main.rs
@@ -1,5 +1,6 @@
+// inspiration was taken from https://github.com/tobyink/advent-of-code/blob/main/2022/15/solution.rs
+
 use regex::Regex;
-use std::collections::HashSet;
 use std::fs;
 
 type Coordinate = (i32, i32);
@@ -8,6 +9,20 @@ type Coordinate = (i32, i32);
 struct Sensor {
     sensor: Coordinate,
     beacon: Coordinate
+}
+
+impl Sensor {
+    fn sensor_beacon_distance(&self) -> i32 {
+        manhattan_dist(&self.sensor, &self.beacon)
+    }
+
+    fn sensor_distance(&self, p: &Coordinate) -> i32 {
+        manhattan_dist(&self.sensor, p)
+    }
+}
+
+fn manhattan_dist(a: &Coordinate, b: &Coordinate) -> i32 {
+    i32::abs(a.0 - b.0) + i32::abs(a.1 - b.1)
 }
 
 fn parse_input(input: &str) -> Vec<Sensor> {
@@ -31,76 +46,58 @@ fn parse_input(input: &str) -> Vec<Sensor> {
     sensors
 }
 
-fn calculate_where_not(sensor: &Sensor, y_target: Option<i32>) -> Vec<Coordinate> {
-    let mut coords = Vec::new();
+fn calculate_where_not_row(sensors: &Vec<Sensor>, x_min: i32, x_max: i32, y: i32) -> i32 {
+    let mut count = 0;
 
-    let manhattan = i32::abs(sensor.sensor.0 - sensor.beacon.0) + i32::abs(sensor.sensor.1 - sensor.beacon.1);
-    let mut rel_y_range = (-manhattan)..(manhattan + 1);
-
-    // to speed things up
-    if let Some(yt) = y_target {
-        let target_rel_y = yt - sensor.sensor.1;
-
-        if !rel_y_range.contains(&target_rel_y) {
-            return coords;
+    for x in x_min..x_max {
+        if sensors.iter().any(|s| s.sensor_distance(&(x, y)) <= s.sensor_beacon_distance()) {
+            count += 1;
         }
 
-        rel_y_range = target_rel_y..(target_rel_y+1);
-    }
-
-    for rel_y in rel_y_range {
-        for rel_x in (-(manhattan - i32::abs(rel_y)))..(manhattan - i32::abs(rel_y) + 1) {
-            coords.push((sensor.sensor.0 + rel_x, sensor.sensor.1 + rel_y));
+        if sensors.iter().any(|s| s.beacon == (x, y)) {
+            count -= 1;
         }
     }
 
-    coords
+    count
 }
 
-fn calculate_where_not_row(sensors: &Vec<Sensor>, y_target: i32) -> usize {
-    let mut set: HashSet<Coordinate> = HashSet::new();
-
+fn get_missing_beacon(sensors: &Vec<Sensor>, from: Coordinate, to: Coordinate) -> Option<Coordinate> {
     for sensor in sensors {
-        set.extend(calculate_where_not(sensor, Some(y_target)).iter())
+        let borders = (0..(sensor.sensor_beacon_distance() + 1)).flat_map(|s| vec![
+            (sensor.sensor.0 + s, sensor.sensor.1 + s - sensor.sensor_beacon_distance() + 1),
+            (sensor.sensor.0 + sensor.sensor_beacon_distance() + 1 - s, sensor.sensor.1 + s),
+            (sensor.sensor.0 - s, sensor.sensor.1 + sensor.sensor_beacon_distance() + 1 - s),
+            (sensor.sensor.0 + s - sensor.sensor_beacon_distance() + 1, sensor.sensor.1 - s)
+        ]).collect::<Vec<Coordinate>>();
+
+        // println!("S({},{}) -> {:?}", sensor.sensor.0, sensor.sensor.1, borders);
+
+        for p in borders {
+            if p.0 < from.0 || p.1 < from.1 || p.0 > to.0 || p.1 > to.1 {
+                continue;
+            }
+
+            if sensors.iter().any(|s1| s1.sensor_distance(&p) <= s1.sensor_beacon_distance()) {
+                continue;
+            }
+
+            return Some(p);
+        }
     }
 
-    for sensor in sensors {
-        set.remove(&sensor.beacon);
-    }
-
-    set.len()
+    None
 }
-
-// fn print_map(sensors: &Vec<Sensor>, from: &Coordinate, to: &Coordinate) {
-//     let where_not = sensors.iter().map(|s| calculate_where_not(s, None)).flatten().collect::<Vec<Coordinate>>();
-
-//     for y in from.1..to.1 {
-//         print!("{:>3}>", y);
-//         for x in from.0..to.0 {
-//             if sensors.iter().any(|s| s.sensor.0 == x && s.sensor.1 == y) {
-//                 print!("S");
-//             } else if sensors.iter().any(|s| s.beacon.0 == x && s.beacon.1 == y) {
-//                 print!("B");
-//             } else if where_not.contains(&(x, y)) {
-//                 print!("#");
-//             } else {
-//                 print!(".");
-//             }
-//         }
-//         println!()
-//     }
-// }
 
 fn main() {
-    // let example = fs::read_to_string("inputs/example.txt").expect("should be able to read input");
-    // let example_sensors = parse_input(&example);
-
-    // print_map(&example_sensors, &(0, 0), &(20, 20));
-
     let input = fs::read_to_string("inputs/input.txt").expect("should be able to read input");
     let sensors = parse_input(&input);
 
-    println!("Part 1: {}", calculate_where_not_row(&sensors, 2000000));
+    println!("Part 1: {}", calculate_where_not_row(&sensors, -2000000, 6000000, 2000000));
+
+    let missing_beacon = get_missing_beacon(&sensors, (0, 0), (4000000, 4000000)).unwrap();
+    let tuning_frequency = ((missing_beacon.0 as i64) * 4000000) + (missing_beacon.1 as i64);
+    println!("Part 2: {}", tuning_frequency);
 }
 
 #[cfg(test)]
@@ -121,28 +118,8 @@ mod tests {
     }
 
     #[test]
-    fn calculate_where_not_example() {
-        assert_eq!(calculate_where_not(&Sensor { sensor: (5, 5), beacon: (6, 7) }, None), vec![
-            (5, 2),
-            (4, 3), (5, 3), (6, 3),
-            (3, 4), (4, 4), (5, 4), (6, 4), (7, 4),
-            (2, 5), (3, 5), (4, 5), (5, 5), (6, 5), (7, 5), (8, 5),
-            (3, 6), (4, 6), (5, 6), (6, 6), (7, 6),
-            (4, 7), (5, 7), (6, 7),
-            (5, 8)
-        ])
-    }
-
-    #[test]
-    fn calculate_where_not_with_target_y_example() {
-        assert_eq!(calculate_where_not(&Sensor { sensor: (5, 5), beacon: (6, 7) }, Some(4)), vec![
-            (3, 4), (4, 4), (5, 4), (6, 4), (7, 4)
-        ])
-    }
-
-    #[test]
     fn calculate_where_not_row_example1() {
-        assert_eq!(calculate_where_not_row(&vec![Sensor { sensor: (8, 7), beacon: (2, 10) }], 10), 12);
+        assert_eq!(calculate_where_not_row(&vec![Sensor { sensor: (8, 7), beacon: (2, 10) }], -20000, 60000, 10), 12);
     }
 
     #[test]
@@ -150,6 +127,14 @@ mod tests {
         let input = fs::read_to_string("inputs/example.txt").expect("should be able to read input");
         let sensors = parse_input(&input);
 
-        assert_eq!(calculate_where_not_row(&sensors, 10), 26);
+        assert_eq!(calculate_where_not_row(&sensors, -20000, 60000, 10), 26);
+    }
+
+    #[test]
+    fn get_missing_beacon_example() {
+        let input = fs::read_to_string("inputs/example.txt").expect("should be able to read input");
+        let sensors = parse_input(&input);
+
+        assert_eq!(get_missing_beacon(&sensors, (0, 0), (20, 20)), Some((14, 11)));
     }
 }
