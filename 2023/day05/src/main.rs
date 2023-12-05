6b89d1bea2e1a35b68bcb75266330bd9d4a3d637
Jake Walker <hi@jakew.me>
2023-12-05 09:01:06 +0000

    day 2 wip

--- 2023/day05/src/main.rs
+++ 2023/day05/src/main.rs
@@ -16,16 +16,67 @@ impl Mapping {
         (self.source_range_start, self.source_range_start+self.range_length-1)
     }
     
-    fn map_number(&self, source: u64) -> Option<u64> {
+    /// map from the source range to the destination range
+    fn map_forward(&self, src: u64) -> Option<u64> {
         let (src_min, src_max) = self.source_range();
         let (dst_min, _)  = self.destination_range();
 
-        if source < src_min || source > src_max {
+        if src < src_min || src > src_max {
             return None;
         }
-        
-        Some((source - src_min) + dst_min)
+
+        Some((src - src_min) + dst_min)
     }
+
+    /// map from the destination range to the source range
+    fn map_backward(&self, dst: u64) -> Option<u64> {
+        let (dst_min, dst_max) = self.destination_range();
+        let (src_min, _) = self.source_range();
+
+        if dst < dst_min || dst > dst_max {
+            return None;
+        }
+
+        Some((dst - dst_min) + src_min)
+    }
+}
+
+fn is_location_possible(mapping_groups: &Vec<Vec<Mapping>>, seed_ranges: &Vec<(u64, u64)>, location: u64) -> bool {
+    let mut current = location;
+
+    for group in mapping_groups.iter().rev() {
+        for mapping in group {
+            if let Some(source) = mapping.map_backward(current) {
+                current = source;
+                break;
+            }
+        }
+    }
+
+    // check if final seed is in ranges
+    for range in seed_ranges {
+        if current >= range.0 && current <= range.1 {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn lowest_location(mapping_groups: Vec<Vec<Mapping>>, seed_ranges: &Vec<(u64, u64)>) -> Option<u64> {
+    let mut last_mappings: &mut Vec<Mapping> = mapping_groups.last().unwrap().clone();
+    last_mappings.sort_by_key(|x| x.destination_range().0);
+
+    for mapping in last_mappings {
+        let (min, max) = mapping.destination_range();
+        for i in min..max {
+            if is_location_possible(&mapping_groups, seed_ranges, i) {
+                return Some(i);
+            }
+        }
+    }
+
+    None
 }
 
 fn map_through(mapping_groups: &Vec<Vec<Mapping>>, seed: &u64) -> u64 {
@@ -33,7 +84,7 @@ fn map_through(mapping_groups: &Vec<Vec<Mapping>>, seed: &u64) -> u64 {
     
     for group in mapping_groups {
         for mapping in group {
-            if let Some(new_seed) = mapping.map_number(seed) {
+            if let Some(new_seed) = mapping.map_forward(seed) {
                 seed = new_seed;
                 break;
             }
@@ -75,6 +126,16 @@ fn parse_mappings(s: &str) -> (Vec<u64>, Vec<Vec<Mapping>>) {
     (seeds, mapping_groups)
 }
 
+fn seeds_to_ranges(seeds: Vec<u64>) -> Vec<(u64, u64)> {
+    let mut ranges: Vec<(u64, u64)> = Vec::new();
+
+    for group in seeds.chunks(2) {
+        ranges.push((group[0], group[0] + group[1] - 1));
+    }
+
+    ranges
+}
+
 fn main() {
     let input = fs::read_to_string("inputs/input.txt").expect("should be able to read input");
     let (seeds, mapping_groups) = parse_mappings(&input);
@@ -114,7 +175,7 @@ mod tests {
         let mapping = Mapping { destination_range_start: 50, source_range_start: 98, range_length: 2 };
         
         assert_eq!(
-            vec![97_u64, 98_u64, 99_u64, 100_u64].into_iter().map(|n| mapping.map_number(n)).collect::<Vec<Option<u64>>>(),
+            vec![97_u64, 98_u64, 99_u64, 100_u64].into_iter().map(|n| mapping.map_forward(n)).collect::<Vec<Option<u64>>>(),
             vec![None, Some(50_u64), Some(51_u64), None]
         );
     }
@@ -128,5 +189,29 @@ mod tests {
         let expected = vec![82, 43, 86, 35];
         
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_seed_to_ranges_example() {
+        assert_eq!(seeds_to_ranges(vec![79, 14]), vec![(79, 92)]);
+    }
+
+    #[test]
+    fn test_is_location_possible() {
+        let input = fs::read_to_string("inputs/example.txt").expect("should be able to read input");
+        let (seeds, mapping_groups) = parse_mappings(&input);
+        let seed_ranges = seeds_to_ranges(seeds);
+
+        assert!(is_location_possible(&mapping_groups, &seed_ranges, 46));
+        assert!(!is_location_possible(&mapping_groups, &seed_ranges, 1));
+    }
+
+    #[test]
+    fn test_lowest_location_example() {
+        let input = fs::read_to_string("inputs/example.txt").expect("should be able to read input");
+        let (seeds, mapping_groups) = parse_mappings(&input);
+        let seed_ranges = seeds_to_ranges(seeds);
+
+        assert_eq!(lowest_location(&mapping_groups, &seed_ranges).unwrap(), 46);
     }
 }
